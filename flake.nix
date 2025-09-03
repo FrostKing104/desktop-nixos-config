032c6fc4230d6e9b484dd557ecea1ceffef413e2
FrostKing104 <frostking104@gmail.com>
2025-09-03 18:44:41 -0400

    Changed hostname in flake.nix to match desktop

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
   outputs = { self, nixpkgs, nix-flatpak, ... }@inputs: {
     # use "nixos", or your hostname as the name of the configuration
     # it's a better practice than "default" shown in the video
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         nix-flatpak.nixosModules.nix-flatpak
