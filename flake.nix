a6fcffdac2ee8950a5c607614cf3f5f4be0175ac
FrostKing104 <frostking104@gmail.com>
2025-09-04 10:40:53 -0400

    First attempt at using Apollo nix flake

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,17 +13,30 @@
     
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
+    # This is the corrected placement.
+    apollo-flake = {
+      url = "github:username/apollo-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nix-flatpak, ... }@inputs: {
-    # use "nixos", or your hostname as the name of the configuration
-    # it's a better practice than "default" shown in the video
+  outputs = { self, nixpkgs, nix-flatpak, apollo-flake, ... }@inputs: {
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
+      specialArgs = {
+        inherit inputs;
+        # You need to define linuxPkgsUnstable here
+        linuxPkgsUnstable = import inputs.nixpkgs-unstable {
+          system = "x86_64-linux";
+        };
+      };
       modules = [
         nix-flatpak.nixosModules.nix-flatpak
         ./configuration.nix
         inputs.home-manager.nixosModules.default
+        (inputs.apollo-flake.nixosModules.${linuxPkgsUnstable.system}.default)
+        ({pkgs, ...}: {
+          services.apollo.package = apollo-flake.packages.${pkgs.system}.default;
+        })
       ];
     };
   };
