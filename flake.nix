656ed607234659006dc1e73a9d9d540c2bfdc476
FrostKing104 <frostking104@gmail.com>
2025-09-04 10:43:45 -0400

    Attempting to fix linuxPkgsUnstable error

--- flake.nix
+++ flake.nix
@@ -3,9 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-
-    nixPkgsUnstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,31 +12,32 @@
     
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
-    # This is the corrected placement.
     apollo-flake = {
       url = "github:username/apollo-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, nix-flatpak, apollo-flake, ... }@inputs: {
-    nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
-      specialArgs = {
-        inherit inputs;
-        # You need to define linuxPkgsUnstable here
-        linuxPkgsUnstable = import inputs.nixpkgs-unstable {
-          system = "x86_64-linux";
-        };
+  outputs = { self, nixpkgs, nix-flatpak, apollo-flake, nixpkgs-unstable, ... }@inputs:
+    let
+      # Define linuxPkgsUnstable here, so it's available to nixosSystem
+      linuxPkgsUnstable = import nixpkgs-unstable {
+        system = "x86_64-linux";
       };
-      modules = [
-        nix-flatpak.nixosModules.nix-flatpak
-        ./configuration.nix
-        inputs.home-manager.nixosModules.default
-        (inputs.apollo-flake.nixosModules.${linuxPkgsUnstable.system}.default)
-        ({pkgs, ...}: {
-          services.apollo.package = apollo-flake.packages.${pkgs.system}.default;
-        })
-      ];
+    in
+    {
+      nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs linuxPkgsUnstable; }; # Pass it to the modules
+        modules = [
+          nix-flatpak.nixosModules.nix-flatpak
+          ./configuration.nix
+          inputs.home-manager.nixosModules.default
+          # This line should now work
+          apollo-flake.nixosModules.${linuxPkgsUnstable.system}.default
+          ({ pkgs, ... }: {
+            services.apollo.package = apollo-flake.packages.${pkgs.system}.default;
+          })
+        ];
+      };
     };
-  };
 }
