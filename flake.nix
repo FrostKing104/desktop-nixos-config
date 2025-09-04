26e2641416154c14d8c51ec384a05b831d5df141
FrostKing104 <frostking104@gmail.com>
2025-09-04 10:42:06 -0400

    Changed name of nixPkgsUnstable

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixPkgsUnstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
