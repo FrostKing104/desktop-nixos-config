20be98a5492cc0709d2c9e5bd7e502c2e5a8edc3
FrostKing104 <frostking104@gmail.com>
2025-09-05 19:02:49 -0400

    Temporarily commented out Obsidian NVF references to focus on base NVF for now

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    obsidian-nvim.url = "github:epwalsh/obsidian.nvim";
+    # obsidian-nvim.url = "github:epwalsh/obsidian.nvim";
     nvf = {
       url = "github:NotAShelf/nvf";
       # You can override the input nixpkgs to follow your system's
@@ -12,7 +12,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
       # Optionally, you can also override individual plugins
       # for example:
-      inputs.obsidian-nvim.follows = "obsidian-nvim"; # <- this will use the obsidian-nvim from your inputs
+      # inputs.obsidian-nvim.follows = "obsidian-nvim"; # <- this will use the obsidian-nvim from your inputs
     };
 
 
