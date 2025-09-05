9cdc8a474a7e427d81acdfa4caf39e630869d955
FrostKing104 <frostking104@gmail.com>
2025-09-05 19:10:47 -0400

    restructured nvim.nix

--- nvim.nix
+++ nvim.nix
@@ -8,16 +8,12 @@
         theme = {
           enable = true;
           name = "catppuccin";
-        }; # A semicolon was needed here to close the 'theme' attribute set.
-        style = "dark"; # The 'style' attribute was incorrectly nested. It should be a direct child of the 'vim' attribute set.
+        };
+        style = "dark";
         statusline.lualine.enable = true;
-      }; # A semicolon was needed here to close the 'vim' attribute set.
-
+      };
       # Fuzzy Finder
       telescope.enable = true;
-      # 'Fancy' Completion Menu
-      autocomplete.nvim-cmp.enable = true;
-
       # Language Support
       languages = {
         enableLSP = true;
@@ -28,7 +24,15 @@
         css.enable = true;
         bash.enable = true;
         shell.enable = true;
-      }; # A semicolon was needed here to close the 'languages' attribute set.
+      };
+    };
+    # The 'autocomplete' option must be under 'plugins'
+    plugins = {
+      autocomplete = {
+        nvim-cmp = {
+          enable = true;
+        };
+      };
     };
   };
 }
