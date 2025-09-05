21f8df6e21e992ccd0c1a63972db6f56e4d8ea72
FrostKing104 <frostking104@gmail.com>
2025-09-05 19:06:14 -0400

    fixed syntax

--- nvim.nix
+++ nvim.nix
@@ -1,32 +1,34 @@
-# ~/nixos-config/nvim.nix
 { pkgs, lib, ... }:
 
 {
-
   programs.nvf = {
     enable = true;
     settings = {
       vim = {
         theme = {
-	  enable = true;
+          enable = true;
           name = "catppuccin";
-          style = "dark";
+        }; # A semicolon was needed here to close the 'theme' attribute set.
+        style = "dark"; # The 'style' attribute was incorrectly nested. It should be a direct child of the 'vim' attribute set.
         statusline.lualine.enable = true;
-	# Fuzzy Finder
-	telescope.enable = true;
-	# 'Fancy' Completion Menu
-	autocomplete.nvim-cmp.enable = true;
-	# Language Support
-        languages = {
-	  enableLSP = true;
-	  enableTreesitter = true;
-	  nix.enable = true;
-	  python.enable = true;
-	  html.enable = true;
-	  css.enable = true;
-	  bash.enable = true;
-	  shell.enable = true;
+      }; # A semicolon was needed here to close the 'vim' attribute set.
+
+      # Fuzzy Finder
+      telescope.enable = true;
+      # 'Fancy' Completion Menu
+      autocomplete.nvim-cmp.enable = true;
+
+      # Language Support
+      languages = {
+        enableLSP = true;
+        enableTreesitter = true;
+        nix.enable = true;
+        python.enable = true;
+        html.enable = true;
+        css.enable = true;
+        bash.enable = true;
+        shell.enable = true;
+      }; # A semicolon was needed here to close the 'languages' attribute set.
     };
   };
-
-};
+}
