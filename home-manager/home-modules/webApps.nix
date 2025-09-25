95e3ca9c9b75035f523d39a952cf9d24083cce68
FrostKing104 <frostking104@gmail.com>
2025-09-25 17:39:17 -0400

    reverted web apps module to clean slate

--- home-manager/home-modules/webApps.nix
+++ home-manager/home-modules/webApps.nix
@@ -1,22 +1,26 @@
 # webApps.nix
 # This file defines all of your browser-based web applications.
+
 { pkgs, ... }:
+
 {
   # define the required packages
   home.packages = [
     pkgs.chromium
     pkgs.papirus-icon-theme
   ];
+
   # Define all your web app desktop entries in this set
   xdg.desktopEntries = {
     "youtube-music" = {
       name = "YouTube Music";
-      exec = "${pkgs.chromium}/bin/chromium --app=https://music.youtube.com --user-data-dir=$HOME/.config/chromium-youtube-music";
+      exec = "${pkgs.chromium}/bin/chromium --app=https://music.youtube.com";
       icon = "youtube-music";
       comment = "YouTube Music streaming service";
       terminal = false;
       type = "Application";
-      categories = [ "AudioVideo" "Network" ];
+      categories = [ "AudioVideo" "Audio" "Network" ];
     };
+
   };
 }
