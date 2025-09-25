0215bd8051c3ab5e040b128c59ebc065e04b8334
FrostKing104 <frostking104@gmail.com>
2025-09-25 17:47:09 -0400

    Reverted YT Webapps. Previous commit worked, but with a unique storage it doesn't sync with my normal chromium profile, causing issues (no extensions, logins, etc.) Might be worth looking into furthur in the future

--- home-manager/home-modules/webApps.nix
+++ home-manager/home-modules/webApps.nix
@@ -1,25 +1,26 @@
 # webApps.nix
 # This file defines all of your browser-based web applications.
 
-# 1. Add `config` to the arguments here
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 
 {
+  # define the required packages
   home.packages = [
     pkgs.chromium
     pkgs.papirus-icon-theme
   ];
 
+  # Define all your web app desktop entries in this set
   xdg.desktopEntries = {
     "youtube-music" = {
       name = "YouTube Music";
-      # 2. Use the Nix variable for the absolute home path
-      exec = "${pkgs.chromium}/bin/chromium --app=https://music.youtube.com --user-data-dir=${config.home.homeDirectory}/.config/chromium-web-apps/youtube-music";
+      exec = "${pkgs.chromium}/bin/chromium --app=https://music.youtube.com";
       icon = "youtube-music";
       comment = "YouTube Music streaming service";
       terminal = false;
       type = "Application";
       categories = [ "AudioVideo" "Audio" "Network" ];
     };
+
   };
 }
