04bdce623cc9b1747f12f8ca2ead93d64d3919ca
FrostKing104 <frostking104@gmail.com>
2025-09-25 17:37:51 -0400

    Changed YT Music Web App for persistent storage + better performance Attemp 2

--- home-manager/home-modules/webApps.nix
+++ home-manager/home-modules/webApps.nix
@@ -1,26 +1,22 @@
 # webApps.nix
 # This file defines all of your browser-based web applications.
-
 { pkgs, ... }:
-
 {
   # define the required packages
   home.packages = [
     pkgs.chromium
     pkgs.papirus-icon-theme
   ];
-
   # Define all your web app desktop entries in this set
   xdg.desktopEntries = {
     "youtube-music" = {
       name = "YouTube Music";
-      exec = "${pkgs.chromium}/bin/chromium --app=https://music.youtube.com --user-data-dir=%h/.config/chromium-youtube-music --enable-features=VaapiVideoDecoder --disable-features=UseChromeOSDirectVideoDecoder --class=youtube-music";
+      exec = "${pkgs.chromium}/bin/chromium --app=https://music.youtube.com --user-data-dir=$HOME/.config/chromium-youtube-music";
       icon = "youtube-music";
       comment = "YouTube Music streaming service";
       terminal = false;
       type = "Application";
-      categories = [ "AudioVideo" "Audio" "Network" ];
+      categories = [ "AudioVideo" "Network" ];
     };
-
   };
 }
