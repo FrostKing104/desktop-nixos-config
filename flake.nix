99a943b98965a5d2730e7d07d303d2bda77e6a1b
FrostKing104 <frostking104@gmail.com>
2025-09-25 22:10:31 -0400

    testing basic nixvim install

--- flake.nix
+++ flake.nix
@@ -9,7 +9,14 @@
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+
+    inputs.nixvim = {
+      # url = "github:nix-community/nixvim";
+      # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+      url = "github:nix-community/nixvim/nixos-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # MOVED: nix-flatpak also goes inside the inputs block
     nix-flatpak.url = "github:gmodena/nix-flatpak";
   
