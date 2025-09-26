b289323b0dd52c901e6de720f5c554c14bb1bbf2
FrostKing104 <frostking104@gmail.com>
2025-09-25 22:17:59 -0400

    stripped down nixvim config for bug testing

--- home-manager/home-modules/nixvim.nix
+++ home-manager/home-modules/nixvim.nix
@@ -5,33 +5,6 @@
   # ⚙️ NixVim Configuration
   programs.nixvim = {
     enable = true;
-    
-    # Basic Configuration Example:
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-
-    # Options (equivalent to :set in Vim/Neovim)
-    opts = {
-      # Sets the number of screen lines to keep above and below the cursor
-      scrolloff = 8; 
-      # Enable mouse support
-      mouse = "a"; 
-      # Highlight the current line
-      cursorline = true;
-    };
-
-    # Plugins Example:
-    plugins = {
-      lualine.enable = true; # Status line
-      treesitter.enable = true; # Syntax highlighting
-    };
-
-    # Colorscheme Example:
-    colorschemes.catppuccin = {
-      enable = true;
-      settings.flavour = "mocha";
-    };
   };
 
 
