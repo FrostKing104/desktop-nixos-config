65e6b453867137c7345ee39cdd4f1c5564daf265
FrostKing104 <frostking104@gmail.com>
2025-09-25 21:23:09 -0400

    removed stupid AI-informed nixvim config

--- flake.nix
+++ flake.nix
@@ -4,15 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    nix-colors = {
-      url = "github:misterio77/nix-colors";
-    };
-
     # MOVED: home-manager now goes inside the inputs block
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
@@ -24,14 +15,13 @@
 
   }; # <- This is the single, correct closing brace for the entire inputs block
 
-  outputs = { self, nixpkgs, nix-flatpak, home-manager, nixvim, nix-colors, ... }@inputs: {
+  outputs = { self, nixpkgs, nix-flatpak, home-manager, ... }@inputs: {
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
         nix-flatpak.nixosModules.nix-flatpak
         ./configuration.nix
         home-manager.nixosModules.default
-        nixvim.nixosModules.nixvim
       ];
     };
   };
