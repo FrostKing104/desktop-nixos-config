aac50cbc8020bd8cc3b35d60141334440ebea7cd
FrostKing104 <frostking104@gmail.com>
2025-09-25 23:01:09 -0400

    configured

--- home-manager/home-modules/nixvim.nix
+++ home-manager/home-modules/nixvim.nix
@@ -16,9 +16,22 @@
 
     plugins = {
 
-    lualine.enable = true;
-
-    indent-blankline.enable = true;
+      indent-blankline = {
+        enable = true;
+        settings.scope = {
+          # Enable the animated scope line (must be a raw Lua function)
+          show_start = config.lib.nixvim.raw "false";
+          show_end = config.lib.nixvim.raw "false";
+          enabled = true;
+          # Use config.lib.nixvim.raw to insert raw Lua code:
+          highlight = config.lib.nixvim.raw "require('catppuccin.groups.integrations.ibl').get()"; 
+          animate = {
+            enabled = true;
+            # 500ms animation duration
+            duration = 500; 
+          };
+        };
+      };
     };
   };
 }
