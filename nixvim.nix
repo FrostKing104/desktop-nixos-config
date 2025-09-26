3b29ff537fdba3b9887510bd06c1780876b0ed72
FrostKing104 <frostking104@gmail.com>
2025-09-25 21:11:30 -0400

    fixed formatting error

--- nixvim.nix
+++ nixvim.nix
@@ -5,19 +5,19 @@
     enable = true;
     defaultEditor = true;
 
-    colorscheme = "catppuccin";
-
-    # ADD "-nvim" TO THE LINE BELOW
-    plugins.catppuccin-nvim = {
+    # This is the correct place for the theme's settings
+    colorschemes.catppuccin = {
       enable = true;
       flavour = "mocha";
     };
 
+    # Basic options
     opts = {
       number = true;
       relativenumber = true;
     };
 
+    # Other plugins (lualine, etc.) still go here
     plugins = {
       lualine.enable = true;
       gitsigns.enable = true;
