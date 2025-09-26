0f3bac94049f152490823bd4f77146620f12cc0a
FrostKing104 <frostking104@gmail.com>
2025-09-25 21:11:09 -0400

    fixed formatting error

--- nixvim.nix
+++ nixvim.nix
@@ -5,24 +5,19 @@
     enable = true;
     defaultEditor = true;
 
-    # DELETE THE LINE BELOW - IT'S INCORRECT AND NOT NEEDED
-    # colorschemes.catppuccin.from = "${inputs.nix-colors}/themes/catppuccin.nix";
-
-    # These settings are all you need for the theme to work.
-    # When you enable the catppuccin plugin, nixvim automatically finds it.
     colorscheme = "catppuccin";
-    plugins.catppuccin = {
+
+    # ADD "-nvim" TO THE LINE BELOW
+    plugins.catppuccin-nvim = {
       enable = true;
       flavour = "mocha";
     };
 
-    # Basic options
     opts = {
       number = true;
       relativenumber = true;
     };
 
-    # Enable a few useful plugins
     plugins = {
       lualine.enable = true;
       gitsigns.enable = true;
