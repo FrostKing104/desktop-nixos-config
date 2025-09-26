d5c65da2160e22dc2979c3006889b8c83ec81da6
FrostKing104 <frostking104@gmail.com>
2025-09-25 22:17:01 -0400

    added reference to home.nix of nixvim.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -8,6 +8,7 @@
     ./home-modules/zsh.nix    
     ./home-modules/fcitx5.nix
     ./home-modules/webApps.nix
+    ./home-modules/nixvim.nix
   ];
 
   # Home Manager needs a bit of information about you and the
