246f60656da74529d87003e81dd106d88f1b3d78
FrostKing104 <frostking104@gmail.com>
2025-09-25 23:41:06 -0400

    syntax

--- home-manager/home-modules/nixvim.nix
+++ home-manager/home-modules/nixvim.nix
@@ -1,69 +1,67 @@
 { config, pkgs, inputs, ... }:
-
 let
   nixvimLib = inputs.nixvim.lib;
 in
 {
   programs.nixvim = {
     enable = true;
-
     colorschemes.catppuccin = {
       enable = true;
       settings = {
         flavour = "mocha";
       };
     };
-
-   {
-  plugins.treesitter = {
-    enable = true;
-    settings = {
-      highlight.enable = true;
-      indent.enable = true;
-    };
-  };
-
-  plugins.mini = {
-    enable = true;
-    modules.indentscope = {
-      symbol = "▎";
-      options = {
-        try_as_border = true;
+    
+    plugins.treesitter = {
+      enable = true;
+      settings = {
+        highlight.enable = true;
+        indent.enable = true;
       };
     };
-  };
-
-  plugins.indent-blankline = {
-    enable = true;
-    settings = {
-      indent = {
-        char = "│";
-        tab_char = "│";
+    
+    plugins.mini = {
+      enable = true;
+      modules.indentscope = {
+        symbol = "▎";
+        options = {
+          try_as_border = true;
+        };
       };
-      scope = {
-        enabled = true;
-        char = "▎";  # Thicker character makes animations more visible
-        show_start = true;
-        show_end = true;
-        show_exact_scope = true;
-        injected_languages = false;
-        highlight = [ "Function" "Label" ];
-        priority = 500;
-      };
-      exclude = {
-        filetypes = [
-          "help"
-          "alpha"
-          "dashboard"
-          "neo-tree"
-          "Trouble"
-          "trouble"
-          "lazy"
-          "mason"
-          "notify"
-          "toggleterm"
-          "lazyterm"
-        ];
+    };
+    
+    plugins.indent-blankline = {
+      enable = true;
+      settings = {
+        indent = {
+          char = "│";
+          tab_char = "│";
+        };
+        scope = {
+          enabled = true;
+          char = "▎";  # Thicker character makes animations more visible
+          show_start = true;
+          show_end = true;
+          show_exact_scope = true;
+          injected_languages = false;
+          highlight = [ "Function" "Label" ];
+          priority = 500;
+        };
+        exclude = {
+          filetypes = [
+            "help"
+            "alpha"
+            "dashboard"
+            "neo-tree"
+            "Trouble"
+            "trouble"
+            "lazy"
+            "mason"
+            "notify"
+            "toggleterm"
+            "lazyterm"
+          ];
+        };
       };
     };
   };
