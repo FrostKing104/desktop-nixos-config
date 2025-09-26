64ab56e13ec39bd438ebde8e3512dc2a5f6ff8c5
FrostKing104 <frostking104@gmail.com>
2025-09-25 22:50:37 -0400

    removed catppuccin options

--- home-manager/home-modules/nixvim.nix
+++ home-manager/home-modules/nixvim.nix
@@ -1,41 +1,19 @@
 { config, pkgs, ... }:
 
 {
+
   # ⚙️ NixVim Configuration
   programs.nixvim = {
-    enable = true; # <-- ADD a comma here, as it's followed by another attribute
+    enable = true;
+  
+  colorschemes.catppuccin = {
+    enable = true;
+    settings = {
+    # Set the 'mocha' flavor
+    flavour = "mocha";
+  };   
 
-    # ✅ CORRECT SCOPE: colorschemes is a sub-attribute of programs.nixvim
-    colorschemes.catppuccin = {
-      # The 'enable' flag for the colorscheme itself
-      enable = true; 
-      disable_underline = true;
-      flavour = "mocha";
-      integrations = {
-        cmp = true;
-        gitsigns = true;
-        mini = {
-          enabled = true;
-          indentscope_color = "";
-        };
-        notify = false;
-        nvimtree = true;
-        treesitter = true;
-      };
-      styles = {
-        booleans = [
-          "bold"
-          "italic"
-        ];
-        conditionals = [
-          "bold"
-        ];
-      };
-      term_colors = true;
-    }; # <--- NO comma needed here, as it's the second-to-last attribute
-    
-    # ✅ CORRECT SCOPE: plugins is also a sub-attribute of programs.nixvim
-    plugins.lualine.enable = true;
-    
-  }; # <-- The programs.nixvim block correctly closes here
+  plugins.lualine.enable = true;
+  
+  };
 }
