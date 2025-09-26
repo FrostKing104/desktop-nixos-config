81d117ab71f8022c1ace568f5d7b50684e36635c
FrostKing104 <frostking104@gmail.com>
2025-09-25 23:39:55 -0400

    mini.intendscope?

--- home-manager/home-modules/nixvim.nix
+++ home-manager/home-modules/nixvim.nix
@@ -14,58 +14,57 @@ in
       };
     };
 
-    plugins = {
-      indent-blankline = {
-        enable = true;
-        settings = {
-          indent = {
-            char = "│";
-            tab_char = "│";
-          };
-          scope = {
-            enabled = true;
-            char = "│";
-            show_start = true;
-            show_end = true;
-            injected_languages = false;
-            highlight = [ "Function" "Label" ];
-            priority = 500;
-          };
-          exclude = {
-            filetypes = [
-              "help"
-              "alpha"
-              "dashboard"
-              "neo-tree"
-              "Trouble"
-              "trouble"
-              "lazy"
-              "mason"
-              "notify"
-              "toggleterm"
-              "lazyterm"
-            ];
-          };
-        };
+   {
+  plugins.treesitter = {
+    enable = true;
+    settings = {
+      highlight.enable = true;
+      indent.enable = true;
+    };
+  };
+
+  plugins.mini = {
+    enable = true;
+    modules.indentscope = {
+      symbol = "▎";
+      options = {
+        try_as_border = true;
       };
     };
-    plugins.treesitter = {
-      enable = true;
+  };
 
-      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-        bash
-        json
-        lua
-        make
-        markdown
-        nix
-        regex
-        toml
-        vim
-        vimdoc
-        xml
-        yaml
-      ];
+  plugins.indent-blankline = {
+    enable = true;
+    settings = {
+      indent = {
+        char = "│";
+        tab_char = "│";
+      };
+      scope = {
+        enabled = true;
+        char = "▎";  # Thicker character makes animations more visible
+        show_start = true;
+        show_end = true;
+        show_exact_scope = true;
+        injected_languages = false;
+        highlight = [ "Function" "Label" ];
+        priority = 500;
+      };
+      exclude = {
+        filetypes = [
+          "help"
+          "alpha"
+          "dashboard"
+          "neo-tree"
+          "Trouble"
+          "trouble"
+          "lazy"
+          "mason"
+          "notify"
+          "toggleterm"
+          "lazyterm"
+        ];
+      };
     };
   };
 }
